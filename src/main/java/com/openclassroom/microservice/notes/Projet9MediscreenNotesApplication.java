6fee4b9292e0807b3d86ad5290d53bb632c86d02
Silvio REA <silvio.rea07@gmail.com>
2022-08-29 11:47:13 +0200

    pom, properties, application

---
+++ src/main/java/com/openclassroom/microservice/notes/Projet9MediscreenNotesApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroom.microservice.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet9MediscreenNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet9MediscreenNotesApplication.class, args);
+	}
+
+}
